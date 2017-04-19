b8e6fe136327b5cec760e5b4a87f957257de4f92
Costa Paraskevopoulos <costa.paras1@gmail.com>
2017-04-19 11:16:42 +1000

    updated simulation.hs

--- src/Simulation.hs
+++ src/Simulation.hs
@@ -1,20 +1,21 @@
+{-
+   Copyright (C) 2017 Costa Paraskevopoulos.
+   Implements functions that progress particle simulation.
+-}
+
 module Simulation (moveParticle, accelerate, advanceWorld) where
   
 import World
 import Physics
 
--- Move a particle according to its velocity for the given number of (simulated) seconds.
---
+-- move a particle according to its velocity for the given number of (simulated) seconds
 moveParticle :: Float -> Particle -> Particle
 moveParticle = error "You need to implement Simulation.moveParticle"
     
--- Accelerate a particle in dependence on the gravitational force excerted by all other particles for
--- the given number of (simulated) seconds.
---
+-- accelerate a particle in dependence on the gravitational force excerted by all other particles for the given number of (simulated) seconds
 accelerate :: Float -> [Particle] -> [Particle]
 accelerate = error "You need to implement Simulation.accelerate"
 
--- Progressing the world state
---
+-- progress the World state
 advanceWorld :: unused -> Float -> World -> World
 advanceWorld = error "You need to implement Simulation.advanceWorld"
